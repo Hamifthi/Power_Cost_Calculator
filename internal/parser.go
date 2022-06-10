bf63f7d96b8fe954d34ef2f4c80c6b427b1537af
hami.fthi <hami.fthi@gmail.com>
2022-06-10 09:38:47 +0430

    for convenience use float64 instead of float32 and also use time type for the start and end in types

--- internal/parser.go
+++ internal/parser.go
@@ -5,22 +5,30 @@ import (
 	"github.com/pkg/errors"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func ParseTariff(lines []string) ([]entity.Tariff, error) {
 	var tariffs []entity.Tariff
 	for i := 0; i < len(lines); i++ {
 		stringSlice := strings.Split(lines[i], ",")
-		energyFee, err := strconv.ParseFloat(stringSlice[2], 32)
+		start, err := time.Parse(time.RFC3339, stringSlice[0])
+		if err != nil {
+			return tariffs, errors.Wrap(err, "Error while parsing Tariffs converting start time string")
+		}
+		end, err := time.Parse(time.RFC3339, stringSlice[1])
+		if err != nil {
+			return tariffs, errors.Wrap(err, "Error while parsing Tariffs converting end time string")
+		}
+		energyFee, err := strconv.ParseFloat(stringSlice[2], 64)
 		if err != nil {
 			return tariffs, errors.Wrap(err, "Error while parsing Tariffs converting energy fee")
 		}
-		parkingFee, err := strconv.ParseFloat(stringSlice[3], 32)
+		parkingFee, err := strconv.ParseFloat(stringSlice[3], 64)
 		if err != nil {
 			return tariffs, errors.Wrap(err, "Error while parsing Tariffs converting parking fee")
 		}
-		tariff := entity.Tariff{Start: stringSlice[0], End: stringSlice[1],
-			EnergyFee: float32(energyFee), ParkingFee: float32(parkingFee)}
+		tariff := entity.Tariff{Start: start, End: end, EnergyFee: energyFee, ParkingFee: parkingFee}
 		tariffs = append(tariffs, tariff)
 	}
 	return tariffs, nil
@@ -30,12 +38,19 @@ func ParseSession(lines []string) ([]entity.Session, error) {
 	var sessions []entity.Session
 	for i := 0; i < len(lines); i++ {
 		stringSlice := strings.Split(lines[i], ",")
-		energy, err := strconv.ParseFloat(stringSlice[3], 32)
+		start, err := time.Parse(time.RFC3339, stringSlice[1])
+		if err != nil {
+			return sessions, errors.Wrap(err, "Error while parsing Sessions converting start time string")
+		}
+		end, err := time.Parse(time.RFC3339, stringSlice[1])
+		if err != nil {
+			return sessions, errors.Wrap(err, "Error while parsing Sessions converting end time string")
+		}
+		energy, err := strconv.ParseFloat(stringSlice[3], 64)
 		if err != nil {
 			return sessions, errors.Wrap(err, "Error while parsing Sessions converting energy")
 		}
-		session := entity.Session{ID: stringSlice[0], Start: stringSlice[1],
-			End: stringSlice[2], Energy: float32(energy)}
+		session := entity.Session{ID: stringSlice[0], Start: start, End: end, Energy: energy}
 		sessions = append(sessions, session)
 	}
 	return sessions, nil
