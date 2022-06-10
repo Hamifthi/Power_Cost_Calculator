9ce332302074c6a5bbea94d6cc8a70b45f71b110
hami.fthi <hami.fthi@gmail.com>
2022-06-10 21:45:36 +0430

    move cost calculator to the arithmetic file

--- internal/arithmethic.go
+++ internal/arithmethic.go
@@ -15,3 +15,23 @@ func CheckTimeOverlap(tariff entity.Tariff, session entity.Session) bool {
 func TruncateFloat(number float64) float64 {
 	return math.Trunc(number*1000) / 1000
 }
+
+func CostCalculator(tariffs []entity.Tariff, sessions []entity.Session) ([]entity.Cost, error) {
+	uniqueCosts := make(map[string]float64)
+	var costs []entity.Cost
+	for _, session := range sessions {
+		for _, tariff := range tariffs {
+			if CheckTimeOverlap(tariff, session) {
+				tariffDuration := tariff.End.Sub(tariff.Start)
+				uniqueCosts[session.ID] = session.Energy*tariff.EnergyFee +
+					tariff.ParkingFee*tariffDuration.Hours()
+			}
+		}
+	}
+	for id, totalCost := range uniqueCosts {
+		uniqueCosts[id] = TruncateFloat(totalCost * 1.15)
+		cost := entity.Cost{SessionID: id, TotalCost: uniqueCosts[id]}
+		costs = append(costs, cost)
+	}
+	return costs, nil
+}
